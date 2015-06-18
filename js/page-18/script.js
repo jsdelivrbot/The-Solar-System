72d2760ed7c80bb5828888e86303268d3d81288c
Marvin Danig <marvin@bubbl.in>
2015-06-18 15:45:27 -0400

    compiled resources of page-18 and uncommitted code of page-17

---
+++ js/page-18/script.js
@@ -0,0 +1,6 @@
+(function() {
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = "https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS_HTML.js";
+  document.getElementsByTagName("head")[0].appendChild(script);
+})();
