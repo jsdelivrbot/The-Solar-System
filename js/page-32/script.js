56714a1576fa2465cea799507d44db79e895e247
Marvin Danig <marvin@bubbl.in>
2015-06-19 09:34:29 -0400

    Page 32-32

---
+++ js/page-32/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  (function() {
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = "https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS_HTML.js";
+    document.getElementsByTagName("head")[0].appendChild(script);
+  })();
+});
