8bd775274c1f7ba318d04ecc2ee08f97dc5a8c16
Marvin Danig <marvin@bubbl.in>
2015-06-19 02:05:20 -0400

    document ready for mathjax pages, resources of page 31

--- js/page-20/script.js
+++ js/page-20/script.js
@@ -1,6 +1,8 @@
-(function() {
-  var script = document.createElement("script");
-  script.type = "text/javascript";
-  script.src = "https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS_HTML.js";
-  document.getElementsByTagName("head")[0].appendChild(script);
-})();
+$(document).ready(function() {
+  (function() {
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = "https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS_HTML.js";
+    document.getElementsByTagName("head")[0].appendChild(script);
+  })();
+});
